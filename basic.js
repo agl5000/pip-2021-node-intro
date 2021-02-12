cb3f9dfc2458b6cb56874958ef25de9cd3e0e52e
Andrew Litts <andrew.litts@gmail.com>
2021-02-12 13:54:43 -0500

    add a launch config

--- basic.js
+++ basic.js
@@ -1,5 +1,10 @@
 const os = require("os"); //built-in node module
+const chance = require("chance");
+
+const myChance = new chance();
 
 console.log("hi");
 
 console.log("It looks like you're running a " + os.platform() + " machine");
+
+console.log("Your new fake name is " + myChance.name());
